c291a9ea6f83fa9189623f06970ebe96e1f62807
RJ Ewing <ewing.rj@gmail.com>
2017-12-28 15:22:27 -1000

    minor bug fix w/ imports/dependencies

--- src/app/views/project/project-expeditions/index.js
+++ src/app/views/project/project-expeditions/index.js
@@ -3,10 +3,10 @@ import fimsExpedition from "../../../components/expeditions/expedition.component
 
 import routing from "./project-expeditions.routes";
 import fimsProjectExpeditions from './project-expeditions.component';
-import fimsProjectExpedition from "./project-expedition";
+import fimsProjectExpedition from "./project-expedition.component";
 
 
-export default angular.module('fims.projectExpeditions', [ fimsProjectExpedition, fimsExpeditions, fimsExpedition ])
+export default angular.module('fims.projectExpeditions', [ fimsExpeditions, fimsExpedition ])
   .run(routing)
   .component('fimsProjectExpeditions', fimsProjectExpeditions)
   .component('fimsProjectExpedition', fimsProjectExpedition)
