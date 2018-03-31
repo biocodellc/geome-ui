2900adb733c97161a1406662c0ff52271eef8456
RJ Ewing <ewing.rj@protonmail.com>
2018-03-31 09:37:03 -0700

    fix webpack warning

--- webpack.config.js
+++ webpack.config.js
@@ -121,7 +121,7 @@ module.exports = (function makeWebpackConfig() {
               // fallbackLoader: 'style-loader',
               // loader: [
               { loader: 'css-loader', options: { sourceMap: true } },
-              { loader: 'postcss-loader' },
+              { loader: 'postcss-loader', options: { sourceMap: true } },
               { loader: 'sass-loader', options: { sourceMap: true } },
             ],
       },
