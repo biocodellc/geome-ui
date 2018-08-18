6b6033dbcd635f119e4862ef1dc00b72ea79872e
mariadeck <m.elenadeck15@gmail.com>
2018-08-18 15:30:21 +0200

    adding text to popups

--- src/app/views/query/QueryMap.js
+++ src/app/views/query/QueryMap.js
@@ -10,17 +10,22 @@ export default class QueryMap extends Map {
     return super.setMarkers(data, this.generatePopupContent.bind(this));
   }
 
-  generatePopupContent({ bcid, genus, species, event }) {
+  generatePopupContent({ bcid, phylum, genus, species, yearCollected, event }) {
     return (
       `<strong>GUID</strong>:  ${bcid}<br>` +
+      `<strong>Phylum</strong>:  ${phylum}<br>` +
       `<strong>Genus</strong>:  ${genus}<br>` +
       `<strong>Species</strong>:  ${species}<br>` +
+      `<strong>Year of collection</strong>:  ${yearCollected}<br>` +
       `<strong>Locality, Country</strong>:  ${event.locality}, ${
         event.country
       }<br>` +
       `<a href='${this.$state.href('record', {
         bcid,
-      })}' target='_blank'>Sample details</a>`
+      })}' target='_blank'>Sample details</a><br>` +
+      `<a href='${this.$state.href('event', {
+        bcid,
+      })}' target='_blank'>Event details</a>`
     );
   }
 }
