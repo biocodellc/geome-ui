40b2ef75ba4d1104645cb402d9da6e13080db280
mariadeck <m.elenadeck15@gmail.com>
2018-09-04 12:51:24 +0200

    password strength meter in 3 locations

--- src/app/components/users/index.js
+++ src/app/components/users/index.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import 'ng-password-strength';
 
 import compareTo from '../../directives/compareTo.directive';
 import routing from './users.routes';
@@ -9,8 +10,9 @@ import newUser from './new-user.component';
 import resetPass from './reset-pass.component';
 
 export default angular
-  .module('fims.users', [compareTo, userService])
+  .module('fims.users', [compareTo, userService, 'ngPasswordStrength'])
   .run(routing)
   .component('fimsProfile', profile)
   .component('fimsNewUser', newUser)
   .component('fimsResetPass', resetPass).name;
+
