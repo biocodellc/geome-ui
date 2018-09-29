e551d8cb1d28a04a6c608ff016fcb16334dd82b9
RJ Ewing <ewing.rj@protonmail.com>
2018-09-29 09:55:51 -0700

    allow attributes to be re-ordered

--- src/app/components/project-config-attributes/index.js
+++ src/app/components/project-config-attributes/index.js
@@ -1,9 +1,14 @@
 import angular from 'angular';
+import 'angular-drag-and-drop-lists';
 import mdDataTable from 'biocode-angular-material-data-table';
 import 'biocode-angular-material-data-table/dist/md-data-table.min.css';
 import fixedTableHeader from '../../directives/fixedTableHeader.directive';
 import projectConfigAttributes from './project-config-attributes.component';
 
 export default angular
-  .module('fims.projectConfigAttributes', [mdDataTable, fixedTableHeader])
+  .module('fims.projectConfigAttributes', [
+    mdDataTable,
+    fixedTableHeader,
+    'dndLists',
+  ])
   .component('fimsProjectConfigAttributes', projectConfigAttributes).name;
