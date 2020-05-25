ce23eacb85558d6702a9c9a4258fb3e3a5add68e
mariadeck <m.elenadeck15@gmail.com>
2020-05-25 07:36:15 +0300

    implements md-table in dashboard view to add sorting functionality

--- src/app/views/dashboard/index.js
+++ src/app/views/dashboard/index.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
 
+// import mdDataTable from 'biocode-angular-material-data-table';
+// import 'biocode-angular-material-data-table/dist/md-data-table.min.css';
 import routing from './dashboard.routes';
 import fimsDashboard from './dashboard.component';
+import projectsTable from './projects-table/projects-table.component';
 
 export default angular
   .module('fims.dashboard', [])
   .run(routing)
-  .component('fimsDashboard', fimsDashboard).name;
+  .component('fimsDashboard', fimsDashboard)
+  .component('projectsTable', projectsTable).name;
