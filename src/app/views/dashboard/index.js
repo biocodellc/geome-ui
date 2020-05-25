a3a259747787815698ee445e06a8a7ae589f1aee
mariadeck <m.elenadeck15@gmail.com>
2020-05-25 16:43:40 +0300

    removes commented code

--- src/app/views/dashboard/index.js
+++ src/app/views/dashboard/index.js
@@ -1,7 +1,5 @@
 import angular from 'angular';
 
-// import mdDataTable from 'biocode-angular-material-data-table';
-// import 'biocode-angular-material-data-table/dist/md-data-table.min.css';
 import routing from './dashboard.routes';
 import fimsDashboard from './dashboard.component';
 import projectsTable from './projects-table/projects-table.component';
