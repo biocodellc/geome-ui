5f6cc7bc3fc423f99a81ac82c7bab572ae022e31
John Deck <jdeck88@gmail.com>
2023-09-08 09:33:54 -0700

    implemented features to query on sample_photos

--- src/app/views/query/query.component.js
+++ src/app/views/query/query.component.js
@@ -29,6 +29,9 @@ class QueryController {
 
     this.showSidebar = true;
     this.showMap = true;
+    this.showTable = false;
+    this.showPhoto = false;
+
     this.loading = false;
     this.sidebarToggleToolTip = 'hide sidebar';
   }
@@ -44,11 +47,11 @@ class QueryController {
         d => d.decimalLatitude && d.decimalLongitude,
       );
       // return table view for advanced queries and queries with no lat lng data, return map view for simple queries
-      if (!hasCoordinates || isAdvancedSearch) {
-        this.toggleMap(false);
-      } else {
-        this.toggleMap(true);
-      }
+      //if (!hasCoordinates || isAdvancedSearch) {
+      //  this.toggleMap(false);
+      //} else {
+      //  this.toggleMap(true);
+     // }
     }
   }
 
@@ -95,9 +98,28 @@ class QueryController {
     this.resizeMap();
   }
 
+  toggleTable(show) {
+    if (show) {
+      this.showTable = true;
+      this.showMap = false;
+      this.showPhoto = false;      
+    }
+  }
+togglePhoto(show) {
+  if (show) {
+    this.showPhoto = true;
+    this.showMap = false;
+    this.showTable = false;
+  }
+}
+
   toggleMap(show) {
     if (this.showMap !== show) this.resizeMap();
-    this.showMap = show;
+    if (show) {
+      this.showMap = true;
+      this.showPhoto = false;
+      this.showTable = false;     
+    }
   }
 
   resizeMap() {
