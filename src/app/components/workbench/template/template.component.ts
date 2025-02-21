881897e04d163328f92a4193d45c16e217fd4fec
SolutionPr <solutions.provider.dev@gmail.com>
2025-02-21 17:52:38 +0530

    Made worksheet and templates dymanic

--- src/app/components/workbench/template/template.component.ts
+++ src/app/components/workbench/template/template.component.ts
@@ -1,14 +1,67 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectService } from '../../../../helpers/services/project.service';
+import { AuthenticationService } from '../../../../helpers/services/authentication.service';
+import { Subject, take, takeUntil } from 'rxjs';
+import { TemplateService } from '../../../../helpers/services/template.service';
 
 @Component({
   selector: 'app-template',
   standalone: true,
-  imports: [CommonModule, NgbAccordionModule],
+  imports: [CommonModule, NgbAccordionModule, RouterLink],
   templateUrl: './template.component.html',
   styleUrl: './template.component.scss'
 })
 export class TemplateComponent {
+  // Injectors
+  projectService = inject(ProjectService);
+  templateService = inject(TemplateService);
+  authService = inject(AuthenticationService);
+  // expeditionService = inject(ExpeditionService);
 
+  // Variables
+  private destroy$ = new Subject<void>();
+  currentUser: any;
+  currentProject: any;
+  isLoading: boolean = true;
+  worksheets: Array<any> = [];
+  allTemplates:Array<any> = [];
+
+  constructor() {
+    this.authService.currentUser.pipe(takeUntil(this.destroy$)).subscribe((res: any) => this.currentUser = res);
+    this.projectService.currentProject$().pipe(takeUntil(this.destroy$)).subscribe((res: any) => {
+      if (res) {
+        console.log('====current project=====', res);
+        this.currentProject = res;
+        this.getTemplates();
+        this.getWorksheets();
+      }
+    })
+  }
+
+  getWorksheets(){
+    this.worksheets = [];
+    this.currentProject.config.entities.forEach((e:any) => {
+      let name = e.worksheet;
+      if (!name || this.worksheets.find(d => d === name)) return;
+      this.worksheets.push(name);
+    });
+    if (this.worksheets.length > 1 && !this.worksheets.includes('Workbook')) {
+      this.worksheets.unshift('Workbook');
+    }
+    console.log('=======worksheets====',this.worksheets)
+  }
+
+  getTemplates(){
+    this.templateService.getAllTempates(this.currentProject.projectId)
+    .pipe(take(1), takeUntil(this.destroy$)).subscribe({
+      next: (res:any)=>{
+        if(res) this.allTemplates = res;
+        this.isLoading = false;
+      },
+      error: ()=> this.isLoading = false
+    })
+  }
 }
---
+++ src/helpers/services/template.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemplateService {
+  // Injectables
+  private http = inject(HttpClient);
+
+  // Variables
+  apiURL:string = environment.restRoot;
+  private headers = new HttpHeaders({
+    'Accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  });
+
+  getAllTempates(projectId:number):Observable<any>{
+    return this.http.get(`${this.apiURL}projects/${projectId}/templates`);
+  }
+
+  generateTempate(projectId:number, worksheetTemplate:any):Observable<any>{
+    return this.http.post(`${this.apiURL}projects/${projectId}/templates/generate`, this.formattedReqBody(worksheetTemplate), { headers: this.headers });
+  }
+
+  saveTempates(projectId:number, templateName:string, data:any):Observable<any>{
+    return this.http.post(`${this.apiURL}projects/${projectId}/templates/${templateName}`, this.formattedReqBody(data), { headers: this.headers });
+  }
+
+  deleteTempates(projectId:number, templateName:string):Observable<any>{
+    return this.http.delete(`${this.apiURL}projects/${projectId}/templates/${templateName}`);
+  }
+
+  // Helper
+  formattedReqBody(body:any):any{
+    return new URLSearchParams(Object.entries(body));
+  }
+}
