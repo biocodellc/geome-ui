cf21aab16686a62f1256337424faa7abfabab6f3
SolutionPr <solutions.provider.dev@gmail.com>
2025-02-24 14:52:59 +0530

    new page design

--- src/app/components/workbench/project-settings/project-settings.component.ts
+++ src/app/components/workbench/project-settings/project-settings.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-project-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgbNavModule],
   templateUrl: './project-settings.component.html',
   styleUrl: './project-settings.component.scss'
 })
 export class ProjectSettingsComponent {
-
+  activeTab:String = 'settings';
 }
