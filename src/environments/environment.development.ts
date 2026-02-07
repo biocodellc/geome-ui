b4b4ebe7c9b06e69ddf7426cde0583b2a0b067e9
John Deck <jdeck88@gmail.com>
2026-02-06 16:20:54 -0800

    adding open to collaborate

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     naan: 21547, //99999,
-    restRoot: 'https://api.geome-db.org/',
+    restRoot: 'http://127.0.0.1:8080/geome-db/',
     appRoot: '/', // When changing this, also need to change <base> tag in index.html
     mapboxToken: 'pk.eyJ1Ijoicm9kbmV5NzU3IiwiYSI6IjAwMjFhMjNkNzkyMmI0ZWE3MmY2YzZmY2NhOGJmMjgwIn0.jUF8Mur9QX59xvbxEOKJYA',
     fimsClientId: 'Rng726w_hMKnvKSH2fUy',
