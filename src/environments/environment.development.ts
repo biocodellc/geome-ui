dc8e4a72327dbc0d31f71f5c4582d734c389a594
John Deck <jdeck88@gmail.com>
2026-02-16 15:13:53 -0800

    Restore tracked Angular environment files for CI build

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  naan: 21547,
+  restRoot: 'http://127.0.0.1:8080/geome-db/',
+  appRoot: '/',
+  mapboxToken: '',
+  fimsClientId: 'Rng726w_hMKnvKSH2fUy',
+  authTimeout: 1000 * 60 * 60 * 24,
+  analyticsId: '',
+  documentationUrl: '/docs/helpDocumentationv3.pdf',
+  sraPageUrl: '/docs/sra-step-by-step-instructions.pdf'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  naan: 21547,
+  restRoot: 'https://api.geome-db.org/',
+  appRoot: '/',
+  mapboxToken: '',
+  fimsClientId: 'Rng726w_hMKnvKSH2fUy',
+  authTimeout: 1000 * 60 * 60 * 24,
+  analyticsId: '',
+  documentationUrl: '/docs/helpDocumentationv4.pdf',
+  sraPageUrl: '/docs/sra-step-by-step-instructions.pdf'
+};
